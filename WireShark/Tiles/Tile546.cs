51b502c2eb2cba73e2a963b87791553c34746bc4
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2025-02-23 11:54:29 -0600

    Added missing tile info for grate on/off.

---
+++ WireShark/Tiles/Tile546.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+
+namespace WireShark.Tiles;
+
+public class Tile546 : TileInfo
+{
+    protected override void HitWireInternal()
+    {
+        {
+            if (Main.tile[i, j - 1] != null && Main.tile[i, j - 1].HasTile)
+            {
+                if (TileID.Sets.BasicChest[Main.tile[i, j - 1].TileType] ||
+                    TileID.Sets.BasicChestFake[Main.tile[i, j - 1].TileType])
+                {
+                    return;
+                }
+
+                if (Main.tile[i, j - 1].TileType == 88)
+                {
+                    return;
+                }
+            }
+
+            tile.TileType = 557;
+            WorldGen.SquareTileFrame(i, j, true);
+        }
+    }
+}
